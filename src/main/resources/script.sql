311eb46476f857088cefd810423a92f334d474f7
vle <obentoka@gmail.com>
2020-03-26 20:52:21 -0400

    added movies to script

---
+++ src/main/resources/script.sql
@@ -0,0 +1,16 @@
+INSERT INTO movies (title, runtime, genre, imdb_score, rating)
+VALUES ('Howard the Duck', 110, 'Sci-Fi', 4.6, 'PG');
+INSERT INTO movies (title, runtime, genre, imdb_score, rating)
+VALUES ('Lavalantula', 83, 'Horror', 4.7, 'TV-14');
+INSERT INTO movies (title, runtime, genre, imdb_score, rating)
+VALUES ('Starship Troopers', 129, 'Sci-Fi', 7.2, 'PG-13');
+INSERT INTO movies (title, runtime, genre, imdb_score, rating)
+VALUES ('Waltz With Bashir', 90, 'Documentary', 8.0, 'R');
+INSERT INTO movies (title, runtime, genre, imdb_score, rating)
+VALUES ('Spaceballs', 96, 'Comedy', 7.1, 'PG');
+INSERT INTO movies (title, runtime, genre, imdb_score, rating)
+VALUES ('Monsters Inc.', 92, 'Animation', 8.1, 'G');
+INSERT INTO movies (title, runtime, genre, imdb_score, rating)
+VALUES ('Your Name', 106, 'Animation', 8.4, 'PG');
+INSERT INTO movies (title, runtime, genre, imdb_score, rating)
+VALUES ('Weathering with You', 112, 'Animation', 7.6, 'PG-13');
