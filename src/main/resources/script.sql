ef02dd6ffc9cd50b641542cb94a9a2415c3e937c
vle <obentoka@gmail.com>
2020-03-26 21:19:15 -0400

    done part 1

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -14,3 +14,13 @@ INSERT INTO movies (title, runtime, genre, imdb_score, rating)
 VALUES ('Your Name', 106, 'Animation', 8.4, 'PG');
 INSERT INTO movies (title, runtime, genre, imdb_score, rating)
 VALUES ('Weathering with You', 112, 'Animation', 7.6, 'PG-13');
+
+SELECT * FROM movies WHERE genre = 'Sci-Fi';
+SELECT * FROM movies WHERE imdb_score > 6.4;
+SELECT * FROM movies WHERE rating IN ('G', 'PG') AND runtime >= 100;
+SELECT AVG(runtime) FROM movies WHERE imdb_score < 7.5 GROUP BY genre;
+UPDATE movies SET rating = 'R' WHERE title = 'Starship Troopers';
+SELECT id, rating from movies WHERE genre IN ('Horror', 'Documentary');
+SELECT AVG(imdb_score), MAX(imdb_score), MIN(imdb_score) from movies GROUP BY rating;
+SELECT rating FROM movies HAVING COUNT(*) > 1;
+DELETE * FROM movies WHERE rating = 'R';
