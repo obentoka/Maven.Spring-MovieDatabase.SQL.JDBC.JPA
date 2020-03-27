11c15c5088a6828a44db883e34d03140b6f41cc2
vle <obentoka@gmail.com>
2020-03-27 17:17:18 -0400

    working on template

--- src/main/java/io/zipcoder/persistenceapp/models/Person.java
+++ src/main/java/io/zipcoder/persistenceapp/models/Person.java
@@ -3,8 +3,6 @@ package io.zipcoder.persistenceapp.models;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
 public class Person {
-    @Id
     Long id;
 }
--- src/main/java/io/zipcoder/persistenceapp/services/PersonService.java
+++ src/main/java/io/zipcoder/persistenceapp/services/PersonService.java
@@ -2,15 +2,14 @@ package io.zipcoder.persistenceapp.services;
 
 import io.zipcoder.persistenceapp.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
 
-    private PersonRepository personRepository;
-
     @Autowired
-    public PersonService(PersonRepository personRepository){
-        this.personRepository = personRepository;
-    }
+    JdbcTemplate jdbcTemplate;
+
+
 }
